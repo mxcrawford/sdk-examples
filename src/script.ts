6252fef7955fa124d8be8445e7a0dd876c5e39ab
Alistair Crawford <alistair.crawford@mendix.com>
2019-10-18 10:41:13 +0100

    Some more cleanup

--- src/script.ts
+++ src/script.ts
@@ -2,36 +2,43 @@ import { MendixSdkClient, OnlineWorkingCopy, Project, loadAsPromise } from 'mend
 import { microflows ,jsonstructures} from 'mendixmodelsdk';
 import when = require("when");
 
+// Configuration
 const config = {
     auth : {
         "username":"alistair.crawford@mendix.com",
         "apikey":"6a867b72-0ca2-46f2-93ab-d1faac5ef12a"
     },
     project : {
-        "projectName": "SDK6",
-        "projectId":"effe3d9a-1bf1-46ae-92f7-f1d2d002d451"
+        "name": "SDK6",
+        "id":"effe3d9a-1bf1-46ae-92f7-f1d2d002d451"
     }
 }
 
+// Use with to get an SDK client
 const client = new MendixSdkClient(config.auth.username, config.auth.apikey);
-var changes = 0;
+
+// Global integer to track any changes that I want to commit at the end
+let changes = 0;
 
 async function main() {
-    const project = new Project(client, config.project.projectId, config.project.projectName);
+    const project = new Project(client, config.project.id, config.project.name);
     const workingCopy = await project.createWorkingCopy();
     processAllMicroflows(workingCopy);
 }
 
-function loadMf(microflow: microflows.IMicroflow): Promise<microflows.Microflow> {
-    return microflow.load();
-}
-
 function processMF(mf: microflows.Microflow, workingCopy: OnlineWorkingCopy) {
+    // Loop through all Microflow objects that are Microflow activities
     mf.objectCollection.objects.filter(mfaction => mfaction.structureTypeName == 'Microflows$ActionActivity')
         .forEach(mfaction => {
+            // We only want ActionActivities here because the others don't have a background colour
             if (mfaction instanceof microflows.ActionActivity) {
+                // Get the action of the Activity to determine it's nature
                 const activity = <microflows.ActionActivity>mfaction;
                 const action = mfaction.action;
+
+                /* If statement to check for each of the types of activities
+                *  Check the colour matches, if not change it and increment the global changes integer 
+                */
                 if (action instanceof microflows.CreateObjectAction) {
                     if (activity.backgroundColor != microflows.ActionActivityColor.Green) {
                         activity.backgroundColor = microflows.ActionActivityColor.Green;
@@ -72,6 +79,7 @@ async function processAllMicroflows(workingCopy: OnlineWorkingCopy) {
             processMF(mf, workingCopy);
         }))
         .done(async () => {
+            // Once processing is done, if there are any changes, we commit the working copy
             if (changes > 0) {
                 console.info("Done MF Processing, made " + changes + " change(s)");
                 const revision = await workingCopy.commit();
--- src/script2.ts
+++ src/script2.ts
@@ -2,66 +2,59 @@ import { MendixSdkClient, OnlineWorkingCopy, Project, loadAsPromise } from 'mend
 import { microflows, projects } from 'mendixmodelsdk';
 import when = require("when");
 
+// Configuration
 const config = {
-    auth : {
-        "username":"alistair.crawford@mendix.com",
-        "apikey":"6a867b72-0ca2-46f2-93ab-d1faac5ef12a"
+    auth: {
+        "username": "alistair.crawford@mendix.com",
+        "apikey": "6a867b72-0ca2-46f2-93ab-d1faac5ef12a"
     },
-    project : {
-        "projectName": "SDK6",
-        "projectId":"effe3d9a-1bf1-46ae-92f7-f1d2d002d451"
+    project: {
+        "name": "SDK6",
+        "id": "effe3d9a-1bf1-46ae-92f7-f1d2d002d451"
     }
 }
 
-let prefixes:string[]= ["IVK_", "ACT_", "SUB_", "WS_" , "ACo_", "ADe_", "BCo_","BDe_"];
+// Microflow Prefix rules
+let prefixes: string[] = ["IVK_", "ACT_", "SUB_", "WS_", "ACo_", "ADe_", "BCo_", "BDe_"];
 
-let moduleList:string[] = ["MyFirstModule"];
+// Modules to process - if left empty, none will be processed
+let moduleList: string[] = ["MyFirstModule"];
 
+// Use auth to create an SDK client
 const client = new MendixSdkClient(config.auth.username, config.auth.apikey);
-let changes = 1;
 
-
-
+// Our list of Microflows that don't adhere to prefix standards
 let prefixMicroflows: string[] = [];
 
+// This is the main function that calls our other functions
 async function main() {
-    const project = new Project(client, config.project.projectId, config.project.projectName);
+    const project = new Project(client, config.project.id, config.project.name);
     const workingCopy = await project.createWorkingCopy();
     processAllMicroflows(workingCopy);
 }
 
-function loadMf(microflow: microflows.IMicroflow): Promise<microflows.Microflow> {
-    return microflow.load();
-}
-
 function processMF(microflow: microflows.Microflow, workingCopy: OnlineWorkingCopy) {
-
     let myContainer = microflow.container;
-    //console.log(myContainer);
-    
-    while(myContainer instanceof projects.Folder)
-    {
+
+    // Recursively get the parent container until the parent is the Module ()
+    while (myContainer instanceof projects.Folder) {
         myContainer = myContainer.container;
     }
 
-    
-    if(myContainer instanceof projects.Module){
+    // Make sure the module is inside our module list - could be done better
+    if (myContainer instanceof projects.Module) {
         let myModule = <projects.Module>myContainer;
-        if(moduleList.find((element) => {return element==myModule.name;})){
-            if(!prefixes.find((element) => {return microflow.name.startsWith(element);})){
+        if (moduleList.find((element) => { return element == myModule.name; })) {
+
+            // Check if the Micrlflow starts with one of our prefixes, if not, add to the naughty list!
+            if (!prefixes.find((element) => { return microflow.name.startsWith(element); })) {
                 prefixMicroflows.push(myModule.name + '.' + microflow.name);
             }
         }
     }
-
-    //console.log(container.toJSON());
-    //const module:projects.Module = realmf.model.;
-
-    
-    
-
 }
 
+// Nice function to load all Microflows using promises 
 function loadAllMicroflowsAsPromise(microflows: microflows.IMicroflow[]): when.Promise<microflows.Microflow[]> {
     return when.all<microflows.Microflow[]>(microflows.map(mf => loadAsPromise(mf)));
 }
@@ -69,20 +62,13 @@ function loadAllMicroflowsAsPromise(microflows: microflows.IMicroflow[]): when.P
 async function processAllMicroflows(workingCopy: OnlineWorkingCopy) {
     loadAllMicroflowsAsPromise(workingCopy.model().allMicroflows())
         .then((microflows) => microflows.forEach((mf) => {
-            //console.log("Processing mf: " + mf.name);
             processMF(mf, workingCopy);
         }))
-        .done(async () => {
-            if (changes > 0) {
-                //console.info("Done MF Processing, made " + changes + " change(s)");
-                //const revision = await workingCopy.commit();
+        .done(async () => { // Once processing is done, display the prefix Microflow list to the console iteratively
                 console.log("Microflows missing prefixes: " + prefixMicroflows.length);
                 prefixMicroflows.forEach((item) => {
                     console.log(item);
                 });
-            } else {
-                //console.info("No changes, skipping commit");
-            }
         });
 }
 
